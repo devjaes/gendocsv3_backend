55145aa68e92307822aa14566e01a111339bf716
devjaes <merajair17@gmail.com>
2025-01-01 17:02:23 -0500

    start templates migration

---
+++ src/gcp/gcp-mtool.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common'
+import processes from './_select_m_code_m_name_p_id_p_nombre_from_model_has_modules_mhm_l_202412310123.json'
+import templates from './plantillas_202412310131.json'
+
+@Injectable()
+export class GcpMToolService {
+  async migrateTemplatesAndProcesses() {}
+}
